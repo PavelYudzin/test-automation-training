6b037fddfcb48c3535f0666a0dc1e8e5d6c30de8
Pavel Yudzin <pavel.yudzinn@gmail.com>
2020-12-08 23:39:28 +0300

    collections optional Task02

---
+++ java/src/main/java/com/company/automation/collections/optional_task/Task02/Task02.java
@@ -0,0 +1,53 @@
+package com.company.automation.collections.optional_task.Task02;
+
+/*
+2.   Ввести число, занести его цифры в стек. Вывести число, у которого цифры идут в обратном порядке.
+ */
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Task02 {
+    public static void main(String[] args) {
+        int number = getInteger();
+        System.out.println("\nOriginal integer: " + number);
+        int revertInteger = getRevertInteger(number);
+        System.out.println("Revert integer: " + revertInteger);
+
+    }
+
+    private static String inputInteger() {
+        String line;
+        String message = "Input integer number:>> ";
+        System.out.print(message);
+        Scanner input = new Scanner(System.in);
+
+        while (!input.hasNextInt()) {
+            System.out.print(message);
+            input.nextLine();
+        }
+
+        line = input.nextLine();
+        return line;
+    }
+
+    private static int getInteger() {
+        String line = inputInteger();
+        return Integer.parseInt(line);
+    }
+
+    private static int getRevertInteger(int number) {
+        char[] digits = Integer.toString(number).toCharArray();
+        Stack<Character> digitsOfInteger = new Stack<>();
+
+        for (char element : digits) {
+            digitsOfInteger.push(element);
+        }
+        StringBuilder sb = new StringBuilder();
+
+        while (!digitsOfInteger.isEmpty()) {
+            sb.append(digitsOfInteger.pop());
+        }
+        return Integer.parseInt(sb.toString());
+    }
+}
