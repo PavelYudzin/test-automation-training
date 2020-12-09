fb4928ea54ab3a023cc0eeef7cb7d28bd56b3192
Pavel Yudzin <pavel.yudzinn@gmail.com>
2020-12-09 23:40:59 +0300

    collections optional Task03

---
+++ java/src/main/java/com/company/automation/collections/optional_task/task03/Task03.java
@@ -0,0 +1,40 @@
+package com.company.automation.collections.optional_task.task03;
+
+/*
+    3.  Создать список из элементов каталога и его подкаталогов.
+ */
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class Task03 {
+    private static final String PATH = new File("src" + File.separator + "main" + File.separator + "java" + File.separator + "com"
+            + File.separator + "company" + File.separator + "automation" + File.separator + "collections" + File.separator
+            + "optional_task" + File.separator + "task03").getAbsolutePath();
+
+    public static void main(String[] args) {
+        ArrayList<String> directoryContent =  getDirectoryContent(PATH);
+        directoryContent.forEach(System.out::println);
+    }
+
+    private static ArrayList<String> getDirectoryContent(String absolutePath) {
+        ArrayList<String> directoryContent = new ArrayList<>();
+
+        File dir = new File(absolutePath);
+        File[] files = dir.listFiles();
+
+        for (File file : files) {
+            if (file.isDirectory()) {
+                directoryContent.add(file.getName());
+            }
+        }
+
+        for (File file : files) {
+            if (file.isFile()) {
+                directoryContent.add(file.getName());
+            }
+        }
+        return directoryContent;
+    }
+}
+
