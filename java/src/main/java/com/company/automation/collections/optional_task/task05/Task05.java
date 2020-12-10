04f4097f48c7f2466c910d9b131c9177c92bdc48
Pavel Yudzin <pavel.yudzinn@gmail.com>
2020-12-10 18:09:04 +0300

    collections optional Task05

---
+++ java/src/main/java/com/company/automation/collections/optional_task/task05/Task05.java
@@ -0,0 +1,17 @@
+package com.company.automation.collections.optional_task.task05;
+
+/*
+    5. 	Не используя вспомогательных объектов, переставить отрицательные элементы данного списка в конец, а положительные — в начало списка.
+ */
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Task05 {
+    public static void main(String[] args) {
+        List<Double> listOfNumbers = Arrays.asList(4.2, -12.65, 24.4, 132.55, -5.47, 213.7, 2.12, 88.2, 34.3, -34.112, 34d);
+        System.out.println("Original list: " + listOfNumbers + "\n");
+        listOfNumbers.sort((n1, n2) -> (int) (n2 - n1));
+        System.out.println("Modified list: " + listOfNumbers);
+    }
+}
